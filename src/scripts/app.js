f7cc3dbde99b862970f9a43e480af9d3f9295b8a
zhuqi0214 <583740616@qq.com>
2017-03-13 11:47:18 +0800

    '第一个版本'

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+/*
+* @Author: iceStone
+* @Date:   2016-01-26 23:10:20
+* @Last Modified by:   iceStone
+* @Last Modified time: 2016-01-26 23:12:46
+*/
+
+'use strict';
+
+console.log('1111');
---
+++ src/scripts/demo.js
@@ -0,0 +1,10 @@
+/*
+* @Author: iceStone
+* @Date:   2016-01-26 23:25:59
+* @Last Modified by:   iceStone
+* @Last Modified time: 2016-01-26 23:26:04
+*/
+
+'use strict';
+
+console.log(222);
